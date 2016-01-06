fb8222bc16b374e01016ff08e259adce9d242210
Kyle Tan <kyle@kyletan.com>
2016-01-06 07:31:22 +0000

    Scrapes exam files too!

--- scraper.py
+++ scraper.py
@@ -14,7 +14,9 @@ with MongoClient() as client:
     local_tz = pytz.timezone("Asia/Singapore")
     coll = client[DB][COLL]
     for user in coll.find():
+        module_codes = set()
         s = requests.Session()
+        # Get course files
         r = s.post("https://ivle.nus.edu.sg/", {
             "ctl00$ctl00$ContentPlaceHolder1$userid": user["username"],
             "ctl00$ctl00$ContentPlaceHolder1$password": user["password"],
@@ -28,11 +30,12 @@ with MongoClient() as client:
         course_ids = set()
         for link in links:
             course_ids.add(link["href"].split("CourseID=")[1])
+            module_codes.add(link.string)
         for course_id in course_ids:
             r = s.get("https://ivle.nus.edu.sg/v1/File/Student/download_all.aspx?CourseID="+course_id)
             soup = BeautifulSoup(r.text, "html.parser")
             if len(soup.find_all(text="No folder found for this Module.")) == 1:
-                continue # Not module found
+                continue # No module found
             module_code = soup.title.string.split(" > ")[1]
             module_name = soup.find("a", class_="accordion-toggle").string
             tree = soup.find("div", class_="TreeView")
@@ -44,12 +47,6 @@ with MongoClient() as client:
                 new_name = " - ".join([module_code, filename.split(".")[0], datestamp + "."+filename.split(".")[1]])
                 # Does it exist on this machine yet?
                 if coll.count({"files.filename": new_name}) == 0:
-                    #os.system("wget -O '{0}/{1}' '{2}{3}'".format(
-                    #    "files",
-                    #    new_name,
-                    #    "https://ivle.nus.edu.sg",
-                    #    url
-                    #    ))
                     with open("{}/{}".format("files", new_name), "wb") as handle:
                         r = s.get("{}{}".format("https://ivle.nus.edu.sg", url), stream=True)
                         if not r.ok:
@@ -57,7 +54,78 @@ with MongoClient() as client:
                         for block in r.iter_content(1024):
                             handle.write(block)
                 # Does this user have this yet?
-                # timestamp = float(subprocess.check_output("stat -c %Y '{}/{}'".format("files", new_name), shell=True))
+                timestamp = os.path.getmtime("{}/{}".format("files", new_name))
+                ts = pytz.utc.localize(datetime.datetime.fromtimestamp(timestamp),
+                    is_dst=None).astimezone(local_tz)
+                if coll.count({ "username": user["username"],
+                    "files.filename": new_name }) == 0:
+                    coll.update({
+                        "username": user["username"]
+                        }, {
+                        "$push": {"files":{
+                            "timestamp": ts,
+                            "timestamp_local": ts.strftime("%Y-%m-%d %H:%M"),
+                            "filename": new_name,
+                            "code": module_code,
+                            "title": module_name,
+                            "status": "new"
+                            }} 
+                        })
+        # Get exam files
+        EXAM_DOMAIN = "https://libbrs.nus.edu.sg"
+        for module_code in module_codes:
+            if (module_code is None) or ("/" in module_code):
+                continue
+            r = s.post(EXAM_DOMAIN + "/infogate/loginAction.do?execution=login", {
+                "userid": user["username"],
+                "password": user["password"],
+                "domain": "NUSSTU",
+                "key": "blankid+RESULT+EXAM+" + module_code
+                })
+            if "Loading page... please wait" not in r.text:
+                continue
+            r = s.post(EXAM_DOMAIN + "/infogate/searchAction.do?execution=ResultList", {
+                "database": "EXAM",
+                "searchstring": module_code,
+                "d": ""
+                })
+            soup = BeautifulSoup(r.text, "html.parser")
+            data = []
+            form = soup.find(attrs={"name": "SearchForm"})
+            for el in form.find_all("input"):
+                attrs = el.attrs
+                if "value" not in attrs:
+                    value = ""
+                else:
+                    value = attrs["value"]
+                if "name" in attrs:
+                    if attrs["name"] == "maxNo":
+                        maxNo = int(value)
+                    elif attrs["name"] == "preSelectedId" or \
+                            attrs["name"] == "exportids":
+                        continue
+                    data.append((attrs["name"], value))
+            if maxNo == 0:
+                continue
+            data.append(("preSelectedId", ",".join([str(i) for i in range(1, maxNo + 1)])))
+            for i in range(1, maxNo+1):
+                data.append(("exportids", str(i)))
+            r = s.post(EXAM_DOMAIN + "/infogate/searchAction.do?execution=ViewSelectedResultListLong", data)
+            soup = BeautifulSoup(r.text, "html.parser")
+            links = soup.find_all("a", string=re.compile("View attached"))
+            for link in links:
+                url = link["href"]
+                filename = link.attrs["title"][14:]
+                new_name = " - ".join([module_code, "EXAM", filename])
+                # Does it exist on this machine yet?
+                if coll.count({"files.filename": new_name}) == 0:
+                    with open("{}/{}".format("files", new_name), "wb") as handle:
+                        r = s.get(EXAM_DOMAIN + "/infogate/" + url, stream=True)
+                        if not r.ok:
+                            print "Oh larddd"
+                        for block in r.iter_content(1024):
+                            handle.write(block)
+                # Does this user have this yet?
                 timestamp = os.path.getmtime("{}/{}".format("files", new_name))
                 ts = pytz.utc.localize(datetime.datetime.fromtimestamp(timestamp),
                     is_dst=None).astimezone(local_tz)
